6e8470cae848b62b40df54f3cfbbdba13c60ca1e
Shifu-in <165253001+Shifu-in@users.noreply.github.com>
2024-06-07 19:27:05 +0700

    Create loading.js

---
+++ loading_screen/loading.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        window.location.href = "../index.html"; // Ссылка на главную страницу
+    }, 4000); // 4 секунды
+});
